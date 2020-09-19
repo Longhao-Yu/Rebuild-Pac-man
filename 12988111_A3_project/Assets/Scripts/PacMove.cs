a25cfdf98a0c4471fff6cf3848afedaed8bea8aa
Longhao-Yu <yuwen349@gmail.com>
2020-09-20 00:39:04 +1000

    level1 and level2
    Imported new visual assets to make pacman move in the map. Next time, you need to add a tween movement.

---
+++ 12988111_A3_project/Assets/Scripts/PacMove.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PacMove : MonoBehaviour
+{
+    float Dirx;
+    float Diry;
+    public int lastTime = 0;
+    private float timer = 0;
+    private float moveTime = 0;
+    private const float moveWait = 2f;
+    Vector2 currD;
+   [SerializeField]
+    private Transform[] transformArray;
+    void Start()
+    {
+        Camera.main.orthographic = true;
+        Camera.main.orthographicSize = 70;
+    }
+
+    void Update()
+    {
+        timer += Time.deltaTime;
+        Dirx = transformArray[0].position.x;  
+        Diry = transformArray[0].position.y;
+        currD = (Vector2)transformArray[0].position;
+        if (timer > (moveTime + moveWait))
+        {
+            moveTime = (int)timer;
+            MoveObjects();
+            Debug.Log(Dirx + "" + Diry);
+        }
+    }
+
+    private void MoveObjects()
+    {
+        //Vector3 temp = Vector3.MoveTowards(transform.position, transformArray[0].position, 2);
+        //GameObject.Find("Pacman").GetComponent<Rigidbody2D>().MovePosition(temp);
+        if (Dirx == 26 && Diry == 26)
+        {
+            transformArray[0].position = new Vector3(26, 10, 0);
+        }
+        else if (Dirx == 26 && Diry == 10)
+        {
+            transformArray[0].position = new Vector3(6, 10, 0);
+        }
+        else if (Dirx == 6 && Diry == 10)
+        {
+            transformArray[0].position = new Vector3(6, 26, 0);
+        }
+        else if (Dirx == 6 && Diry == 26)
+        {
+            transformArray[0].position = new Vector3(26, 26, 0);
+        }
+        Vector2 dir = (Vector2)transformArray[0].position - currD;
+        GameObject.Find("Pacman").GetComponent<Animator>().SetFloat("DirX", dir.x);
+        GameObject.Find("Pacman").GetComponent<Animator>().SetFloat("DirY", dir.y);
+    }
+}
---
+++ 12988111_A3_project/Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    public float speed;
+    private Vector2 dest = Vector2.zero;
+    void Start()
+    {
+        speed = 0.5f;//移动速度为0.5
+        dest = this.transform.position;//把pacman当前位置存储到dest
+    }
+
+    void Update()
+    {
+        
+    }
+    private void FixedUpdate()
+    {
+        Vector2 temp =  Vector2.MoveTowards(transform.position,dest, speed);//使用补间运动将pacman从当前位置移动到dest
+        Debug.Log("目的地： " + dest + "" + "当前位置: " + transform.position);
+        GetComponent<Rigidbody2D>().MovePosition(temp);
+        if ((Vector2)transform.position == dest || Valid(Vector2.up))
+        {
+            if (Input.GetKey(KeyCode.UpArrow) && Valid(Vector2.up))
+            {
+                dest = (Vector2)transform.position + Vector2.up * 4;//目的地为当前位置加上移动的方向上的位置
+            }
+            if (Input.GetKey(KeyCode.DownArrow) && Valid(Vector2.down))
+            {
+                dest = (Vector2)transform.position + Vector2.down * 4;
+            }
+            if (Input.GetKey(KeyCode.LeftArrow) && Valid(Vector2.left))
+            {
+                dest = (Vector2)transform.position + Vector2.left * 4;
+            }
+            if (Input.GetKey(KeyCode.RightArrow) && Valid(Vector2.right))
+            {
+                dest = (Vector2)transform.position + Vector2.right * 4;
+            }
+            Vector2 dir = dest - (Vector2)transform.position;//目的地位置减去现在的位置
+            GetComponent<Animator>().SetFloat("DirX", dir.x);//如果目的地减现在位置的x大于1，说明pacman向右在走
+            GetComponent<Animator>().SetFloat("DirY", dir.y);
+        }
+    } 
+
+    private bool Valid(Vector2 dir)
+    {
+        Vector2 pos = transform.position;//存储现在位置
+        RaycastHit2D hit = Physics2D.Linecast(pos + dir, pos);//发射一道激光，从要去的pos+dir位置到pos
+        return hit.collider == GetComponent<Collider2D>();//如果碰到的是pacman的collider碰撞体，说明没有阻挡
+    } 
+}
